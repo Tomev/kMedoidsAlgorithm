42ca8630c7c463a832461b1897fe332277eb105c
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-03-14 13:22:47 +0100

    Exp467.

--- groupingAlgorithm/cluster.h
+++ groupingAlgorithm/cluster.h
@@ -57,7 +57,7 @@ class cluster
 
     // Prediction
     std::vector<double> predictionParameters;
-    double _deactualizationParameter = 0.98; // w_a -- w, since first article
+    double _deactualizationParameter = 0.99; // w_a -- w, since first article
     int _predictionsSteps = 1;
     int _j = 0;
     std::vector<std::vector<double>> _matrixDj = {};
